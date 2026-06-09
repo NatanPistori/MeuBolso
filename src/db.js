618eaf329bb0da277f25a698a9ca1f51313e6b50
NatanPistori <101145389+NatanPistori@users.noreply.github.com>
2026-06-09 14:29:24 -0300

    Add SSL configuration to database connection

--- src/db.js
+++ src/db.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+  timezone: '+00:00',
   waitForConnections: true,
   connectionLimit: 10,
-  timezone: '+00:00',
 });
 
 module.exports = pool;
+
